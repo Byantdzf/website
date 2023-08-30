de7dd8fafe3168533afc201d586f94086dbd60b6
mamba <dengzhifeng_63@613.com>
2023-08-30 17:27:14 +0800

    测试

---
+++ assets/Software_bigData-79add7be.js
@@ -0,0 +1,1 @@
+import{_ as e,c as t,o}from"./index-7c08ddd3.js";const c={},r={id:"Software"};function a(_,n){return o(),t("div",r,"大数据管理系统")}const f=e(c,[["render",a]]);export{f as default};
---
+++ assets/Software_smartTown-03c42803.js
@@ -0,0 +1,1 @@
+import{_ as e,c as o,o as t}from"./index-7c08ddd3.js";const c={},r={id:"smartTown"};function n(s,_){return t(),o("div",r,"智能小镇管理系统")}const f=e(c,[["render",n]]);export{f as default};
